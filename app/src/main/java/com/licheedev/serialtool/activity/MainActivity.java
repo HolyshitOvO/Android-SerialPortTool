31698d67ff0d928e75f4ecf7198c1bd41aa92f2f
HolyshitOvO <39211450+HolyshitOvO@users.noreply.github.com>
2023-04-04 15:30:19 +0800

    2023-04-04

--- app/src/main/java/com/licheedev/serialtool/activity/MainActivity.java
+++ app/src/main/java/com/licheedev/serialtool/activity/MainActivity.java
@@ -25,185 +25,185 @@ import static com.licheedev.serialtool.R.array.baudrates;
 
 public class MainActivity extends BaseActivity implements AdapterView.OnItemSelectedListener {
 
-    @BindView(R.id.spinner_devices)
-    Spinner mSpinnerDevices;
-    @BindView(R.id.spinner_baudrate)
-    Spinner mSpinnerBaudrate;
-    @BindView(R.id.btn_open_device)
-    Button mBtnOpenDevice;
-    @BindView(R.id.btn_send_data)
-    Button mBtnSendData;
-    @BindView(R.id.btn_load_list)
-    Button mBtnLoadList;
-    @BindView(R.id.et_data)
-    EditText mEtData;
+	@BindView(R.id.spinner_devices)
+	Spinner mSpinnerDevices;
+	@BindView(R.id.spinner_baudrate)
+	Spinner mSpinnerBaudrate;
+	@BindView(R.id.btn_open_device)
+	Button mBtnOpenDevice;
+	@BindView(R.id.btn_send_data)
+	Button mBtnSendData;
+	@BindView(R.id.btn_load_list)
+	Button mBtnLoadList;
+	@BindView(R.id.et_data)
+	EditText mEtData;
 
-    private Device mDevice;
+	private Device mDevice;
 
-    private int mDeviceIndex;
-    private int mBaudrateIndex;
+	private int mDeviceIndex;
+	private int mBaudrateIndex;
 
-    private String[] mDevices;
-    private String[] mBaudrates;
+	private String[] mDevices;
+	private String[] mBaudrates;
 
-    private boolean mOpened = false;
+	private boolean mOpened = false;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        mEtData.setTransformationMethod(new AllCapTransformationMethod(true));
+		mEtData.setTransformationMethod(new AllCapTransformationMethod(true));
 
-        initDevice();
-        initSpinners();
-        updateViewState(mOpened);
-    }
+		initDevice();
+		initSpinners();
+		updateViewState(mOpened);
+	}
 
-    @Override
-    protected void onDestroy() {
-        SerialPortManager.instance().close();
-        super.onDestroy();
-    }
+	@Override
+	protected void onDestroy() {
+		SerialPortManager.instance().close();
+		super.onDestroy();
+	}
 
-    @Override
-    protected boolean hasActionBar() {
-        return false;
-    }
+	@Override
+	protected boolean hasActionBar() {
+		return false;
+	}
 
-    @Override
-    protected int getLayoutId() {
-        return R.layout.activity_main;
-    }
+	@Override
+	protected int getLayoutId() {
+		return R.layout.activity_main;
+	}
 
-    /**
-     * 初始化设备列表
-     */
-    private void initDevice() {
+	/**
+	 * 初始化设备列表
+	 */
+	private void initDevice() {
 
-        SerialPortFinder serialPortFinder = new SerialPortFinder();
+		SerialPortFinder serialPortFinder = new SerialPortFinder();
 
-        // 设备
-        mDevices = serialPortFinder.getAllDevicesPath();
-        if (mDevices.length == 0) {
-            mDevices = new String[] {
-                getString(R.string.no_serial_device)
-            };
-        }
-        // 波特率
-        mBaudrates = getResources().getStringArray(baudrates);
+		// 设备
+		mDevices = serialPortFinder.getAllDevicesPath();
+		if (mDevices.length == 0) {
+			mDevices = new String[] {
+					getString(R.string.no_serial_device)
+			};
+		}
+		// 波特率
+		mBaudrates = getResources().getStringArray(baudrates);
 
-        mDeviceIndex = PrefHelper.getDefault().getInt(PreferenceKeys.SERIAL_PORT_DEVICES, 0);
-        mDeviceIndex = mDeviceIndex >= mDevices.length ? mDevices.length - 1 : mDeviceIndex;
-        mBaudrateIndex = PrefHelper.getDefault().getInt(PreferenceKeys.BAUD_RATE, 0);
+		mDeviceIndex = PrefHelper.getDefault().getInt(PreferenceKeys.SERIAL_PORT_DEVICES, 0);
+		mDeviceIndex = mDeviceIndex >= mDevices.length ? mDevices.length - 1 : mDeviceIndex;
+		mBaudrateIndex = PrefHelper.getDefault().getInt(PreferenceKeys.BAUD_RATE, 0);
 
-        mDevice = new Device(mDevices[mDeviceIndex], mBaudrates[mBaudrateIndex]);
-    }
+		mDevice = new Device(mDevices[mDeviceIndex], mBaudrates[mBaudrateIndex]);
+	}
 
-    /**
-     * 初始化下拉选项
-     */
-    private void initSpinners() {
+	/**
+	 * 初始化下拉选项
+	 */
+	private void initSpinners() {
 
-        ArrayAdapter<String> deviceAdapter =
-            new ArrayAdapter<String>(this, R.layout.spinner_default_item, mDevices);
-        deviceAdapter.setDropDownViewResource(R.layout.spinner_item);
-        mSpinnerDevices.setAdapter(deviceAdapter);
-        mSpinnerDevices.setOnItemSelectedListener(this);
+		ArrayAdapter<String> deviceAdapter =
+				new ArrayAdapter<String>(this, R.layout.spinner_default_item, mDevices);
+		deviceAdapter.setDropDownViewResource(R.layout.spinner_item);
+		mSpinnerDevices.setAdapter(deviceAdapter);
+		mSpinnerDevices.setOnItemSelectedListener(this);
 
-        ArrayAdapter<String> baudrateAdapter =
-            new ArrayAdapter<String>(this, R.layout.spinner_default_item, mBaudrates);
-        baudrateAdapter.setDropDownViewResource(R.layout.spinner_item);
-        mSpinnerBaudrate.setAdapter(baudrateAdapter);
-        mSpinnerBaudrate.setOnItemSelectedListener(this);
+		ArrayAdapter<String> baudrateAdapter =
+				new ArrayAdapter<String>(this, R.layout.spinner_default_item, mBaudrates);
+		baudrateAdapter.setDropDownViewResource(R.layout.spinner_item);
+		mSpinnerBaudrate.setAdapter(baudrateAdapter);
+		mSpinnerBaudrate.setOnItemSelectedListener(this);
 
-        mSpinnerDevices.setSelection(mDeviceIndex);
-        mSpinnerBaudrate.setSelection(mBaudrateIndex);
-    }
+		mSpinnerDevices.setSelection(mDeviceIndex);
+		mSpinnerBaudrate.setSelection(mBaudrateIndex);
+	}
 
-    @OnClick({ R.id.btn_open_device, R.id.btn_send_data, R.id.btn_load_list })
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            case R.id.btn_open_device:
-                switchSerialPort();
-                break;
-            case R.id.btn_send_data:
-                sendData();
-                break;
-            case R.id.btn_load_list:
-                startActivity(new Intent(this, LoadCmdListActivity.class));
-                break;
-        }
-    }
+	@OnClick({ R.id.btn_open_device, R.id.btn_send_data, R.id.btn_load_list })
+	public void onViewClicked(View view) {
+		switch (view.getId()) {
+			case R.id.btn_open_device:
+				switchSerialPort();
+				break;
+			case R.id.btn_send_data:
+				sendData();
+				break;
+			case R.id.btn_load_list:
+				startActivity(new Intent(this, LoadCmdListActivity.class));
+				break;
+		}
+	}
 
-    private void sendData() {
+	private void sendData() {
 
-        String text = mEtData.getText().toString().trim();
-        if (TextUtils.isEmpty(text) || text.length() % 2 != 0) {
-            ToastUtil.showOne(this, "无效数据");
-            return;
-        }
+		String text = mEtData.getText().toString().trim();
+		if (TextUtils.isEmpty(text) || text.length() % 2 != 0) {
+			ToastUtil.showOne(this, "无效数据");
+			return;
+		}
 
-        SerialPortManager.instance().sendCommand(text);
-    }
+		SerialPortManager.instance().sendCommand(text);
+	}
 
-    /**
-     * 打开或关闭串口
-     */
-    private void switchSerialPort() {
-        if (mOpened) {
-            SerialPortManager.instance().close();
-            mOpened = false;
-        } else {
+	/**
+	 * 打开或关闭串口
+	 */
+	private void switchSerialPort() {
+		if (mOpened) {
+			SerialPortManager.instance().close();
+			mOpened = false;
+		} else {
 
-            // 保存配置
-            PrefHelper.getDefault().saveInt(PreferenceKeys.SERIAL_PORT_DEVICES, mDeviceIndex);
-            PrefHelper.getDefault().saveInt(PreferenceKeys.BAUD_RATE, mBaudrateIndex);
+			// 保存配置
+			PrefHelper.getDefault().saveInt(PreferenceKeys.SERIAL_PORT_DEVICES, mDeviceIndex);
+			PrefHelper.getDefault().saveInt(PreferenceKeys.BAUD_RATE, mBaudrateIndex);
 
-            mOpened = SerialPortManager.instance().open(mDevice) != null;
-            if (mOpened) {
-                ToastUtil.showOne(this, "成功打开串口");
-            } else {
-                ToastUtil.showOne(this, "打开串口失败");
-            }
-        }
-        updateViewState(mOpened);
-    }
+			mOpened = SerialPortManager.instance().open(mDevice) != null;
+			if (mOpened) {
+				ToastUtil.showOne(this, "成功打开串口");
+			} else {
+				ToastUtil.showOne(this, "打开串口失败");
+			}
+		}
+		updateViewState(mOpened);
+	}
 
-    /**
-     * 更新视图状态
-     *
-     * @param isSerialPortOpened
-     */
-    private void updateViewState(boolean isSerialPortOpened) {
+	/**
+	 * 更新视图状态
+	 *
+	 * @param isSerialPortOpened
+	 */
+	private void updateViewState(boolean isSerialPortOpened) {
 
-        int stringRes = isSerialPortOpened ? R.string.close_serial_port : R.string.open_serial_port;
+		int stringRes = isSerialPortOpened ? R.string.close_serial_port : R.string.open_serial_port;
 
-        mBtnOpenDevice.setText(stringRes);
+		mBtnOpenDevice.setText(stringRes);
 
-        mSpinnerDevices.setEnabled(!isSerialPortOpened);
-        mSpinnerBaudrate.setEnabled(!isSerialPortOpened);
-        mBtnSendData.setEnabled(isSerialPortOpened);
-        mBtnLoadList.setEnabled(isSerialPortOpened);
-    }
+		mSpinnerDevices.setEnabled(!isSerialPortOpened);
+		mSpinnerBaudrate.setEnabled(!isSerialPortOpened);
+		mBtnSendData.setEnabled(isSerialPortOpened);
+		mBtnLoadList.setEnabled(isSerialPortOpened);
+	}
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-        // Spinner 选择监听
-        switch (parent.getId()) {
-            case R.id.spinner_devices:
-                mDeviceIndex = position;
-                mDevice.setPath(mDevices[mDeviceIndex]);
-                break;
-            case R.id.spinner_baudrate:
-                mBaudrateIndex = position;
-                mDevice.setBaudrate(mBaudrates[mBaudrateIndex]);
-                break;
-        }
-    }
+		// Spinner 选择监听
+		switch (parent.getId()) {
+			case R.id.spinner_devices:
+				mDeviceIndex = position;
+				mDevice.setPath(mDevices[mDeviceIndex]);
+				break;
+			case R.id.spinner_baudrate:
+				mBaudrateIndex = position;
+				mDevice.setBaudrate(mBaudrates[mBaudrateIndex]);
+				break;
+		}
+	}
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-        // 空实现
-    }
+	@Override
+	public void onNothingSelected(AdapterView<?> parent) {
+		// 空实现
+	}
 }
